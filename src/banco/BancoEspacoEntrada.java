517878f86306e396c81bfffc5f1fd8c246e4075c
Leonardo Neves <enfimleo@gmail.com>
2016-07-27 02:40:08 -0300

    bugfix , correcao dos erros

--- src/banco/BancoEspacoEntrada.java
+++ src/banco/BancoEspacoEntrada.java
@@ -24,54 +24,18 @@ public class BancoEspacoEntrada {
 	}
 	
 
-	//com espera menor do que 20s
+	//F1b1 (1<=caixas<=10)
 	@Test
-	public void testemenos() {
-		int[][] tempos = {{1,10},{10,10}};
-		int result = Banco.banco(1, 2, tempos);
-		assertEquals(0, result);
-	}
-	
-	//com espera igual a 20s
-	@Test
-	public void testeigual() {
-		int[][] tempos = {{0,10},{0,10},{0,1}};
-		int result = Banco.banco(1, 3, tempos);
-		assertEquals(0, result);
-	}
-	
-	//com espera maior do que 20s
-	@Test
-	public void testemais() {
-		int[][] tempos = {{1,7},{1,7},{1,7},{1,10}};
+	public void com_espera() {
+		int[][] tempos = { { 0, 10 }, { 0, 10 }, { 3, 8 } ,{4,5}};
 		int result = Banco.banco(1, 4, tempos);
 		assertEquals(1, result);
 	}
-	
-	
-	// caixas < 0
-	// retorno esperado 0, o codigo nao espera valores de entrada fora dos limites
-	@Test
-	public void caixasmenor0() {
-		int[][] tempos = {{ 1, 1 }};
-		int result = Banco.banco(-1, 1, tempos);
-		assertEquals(0, result);
-	}
-	
-	
-	
-	// clientes < 0
-//	 retorno esperado 0, o codigo nao espera valores de entrada fora dos limites
-//	@Test
-//	public void clientesmenor0() {
-//		int[][] tempos = null;
-//		int result = Banco.banco(1, -1, tempos);
-//		assertEquals(0, result);
-//	}
 
-	
-	
-	// caixas > 10
+		
+	//F1b2 (caixas>10)
+	//Impossível - Restrição das entradas do problema
+
 	// retorno esperado depende dos valores de tempos, 
 	//o codigo nao espera valores de entrada fora dos limites
 	@Test
@@ -80,51 +44,52 @@ public class BancoEspacoEntrada {
 		int result = Banco.banco(20, 1, tempos);
 		assertEquals(0, result);
 	}
-	
-	
-	
-	// clientes > 1000
-	// retorno esperado depende dos valores de tempos,
-	//o codigo nao espera valores de entrada fora dos limites
+		
+
+		
+	//F1b3 (caixas<1)
+	//Impossível - Restrição das entradas do problema
+		// retorno esperado 0, o codigo nao espera valores de entrada fora dos limites
+		@Test
+		public void caixasmenor0() {
+			int[][] tempos = {{ 1, 1 }};
+			int result = Banco.banco(-1, 1, tempos);
+			assertEquals(0, result);
+		}
+		
+
+	//F2b1  (1<=clientes<=1000
 	@Test
-	public void clientesmaior1000() {
-		int caixas = 3;
-		int clientes = 1010;
-		int[][] tempos = temposrandom(clientes);
-		int result = Banco.banco(caixas, clientes, tempos);
-		assertTrue(result>900);
-	}
-	
-	
-	
-	//caixas=clientes
-	@Test
-	public void iguais() {
-		int[][] tempos = { { 1, 2 }, { 1, 2 }, { 3, 4 } };
-		int result = Banco.banco(3, 3, tempos);
-		assertEquals(0, result);
-	}
-	
-	
-	
-	//caixas<clientes
-	@Test
-	public void maisclientes() {
-		int[][] tempos = { { 1, 2 }, { 1, 2 }, { 3, 4 } };
+	public void testeigual() {
+		int[][] tempos = {{0,10},{0,10},{0,1}};
 		int result = Banco.banco(1, 3, tempos);
 		assertEquals(0, result);
 	}
-	
-	//caixas<clientes com espera
+
+		
+	//F2b2 (clientes>1000)
+	//Impossível - Restrição das entradas do problema
+		@Test
+		public void clientesmaior1000() {
+			int caixas = 3;
+			int clientes = 1010;
+			int[][] tempos = temposrandom(clientes);
+			int result = Banco.banco(caixas, clientes, tempos);
+			assertTrue(result>900);
+		}
+
+	//F2b3 (clientes<1)
+	//Impossível - Restrição das entradas do problema
+	// retorno esperado 0, o codigo nao espera valores de entrada fora dos limites
 	@Test
-	public void com_espera() {
-		int[][] tempos = { { 0, 10 }, { 0, 10 }, { 3, 8 } ,{4,5}};
-		int result = Banco.banco(1, 4, tempos);
-		assertEquals(1, result);
+	public void clientesmenor0() {
+		int[][] tempos = null;
+		int result = Banco.banco(1, -1, tempos);
+		assertEquals(0, result);
 	}
-	
-	
-	//caixas>clientes
+
+		
+	//F3b1 (caixas > clientes)
 	@Test
 	public void maiscaixas() {
 		int clientes = 20;
@@ -132,6 +97,20 @@ public class BancoEspacoEntrada {
 		int result = Banco.banco(caixas, clientes, temposrandom(clientes));
 		assertEquals(0, result);
 	}
-	
+
+	//F3b2 (caixas < clientes)
+	public void test1() {
+		int[][] tempos = {{1,2},{1,2}};
+		int result = Banco.banco(1, 2, tempos);
+		assertEquals(0, result);
+	}
+		
+	//F3b3 (caixas == clientes)
+	@Test
+	public void iguais() {
+		int[][] tempos = { { 1, 2 }, { 1, 2 }, { 3, 4 } };
+		int result = Banco.banco(3, 3, tempos);
+		assertEquals(0, result);
+	}
 
 }
